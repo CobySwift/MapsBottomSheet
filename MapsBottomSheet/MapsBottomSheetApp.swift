7fde9d90f1d5840b823ea30bb05417263c4a184a
coby5502 <57849386+coby5502@users.noreply.github.com>
2023-07-31 09:58:01 +0900

    Initial Commit

---
+++ MapsBottomSheet/MapsBottomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsBottomSheetApp.swift
+//  MapsBottomSheet
+//
+//  Created by COBY_PRO on 2023/07/31.
+//
+
+import SwiftUI
+
+@main
+struct MapsBottomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
