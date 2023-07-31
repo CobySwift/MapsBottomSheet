54e69a95aef50eb1538115252c2f06b85622ab9f
coby5502 <57849386+coby5502@users.noreply.github.com>
2023-07-31 11:41:30 +0900

    [FIX] 오류 수정

--- MapsBottomSheet/View/Helpers/Extensions.swift
+++ MapsBottomSheet/View/Helpers/Extensions.swift
@@ -30,29 +30,32 @@ extension View {
                     .presentationDetents(presentationDetents)
                     .presentationDragIndicator(dragIndicator)
                     .interactiveDismissDisabled(interactiveDisabled)
+                    .presentationBackgroundInteraction(.enabled) // iOS 16.4
                     .onAppear {
                         // MARK: Custom Code For Bottom Sheet
                         // Finding the Presented View Controller
                         guard let windows = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return }
                         
-                        if let controller = windows.windows.first?.rootViewController?.presentedViewController,
-                           let sheet = controller.presentationController as? UISheetPresentationController {
-                            
-                            // FOR TRANSPARENT BACKGROUND
-                            if isTransparentBG {
-                                controller.view.backgroundColor = .clear
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                            if let controller = windows.windows.first?.rootViewController?.presentedViewController,
+                               let sheet = controller.presentationController as? UISheetPresentationController {
+                                
+                                // FOR TRANSPARENT BACKGROUND
+                                if isTransparentBG {
+                                    controller.view.backgroundColor = .clear
+                                }
+                                
+                                // FROM THIS EXTRACTING PRESENTATION CONTROLLER
+                                // SOMETIMES BUTTONS AND ACTIONS WILL BE TINTED IN HIDDEN FORM
+                                // TO AVOID THIS
+                                controller.presentingViewController?.view.tintAdjustmentMode = .normal
+                                
+                                // MARK: As Usual Set Properties What Ever Your Wish Here With Sheet Controller
+                                sheet.largestUndimmedDetentIdentifier = largestUndimmedIdentifier
+                                sheet.preferredCornerRadius = sheetCornerRadius
+                            } else {
+                                print("NO CONTROLLER FOUND")
                             }
-                            
-                            // FROM THIS EXTRACTING PRESENTATION CONTROLLER
-                            // SOMETIMES BUTTONS AND ACTIONS WILL BE TINTED IN HIDDEN FORM
-                            // TO AVOID THIS
-                            controller.presentingViewController?.view.tintAdjustmentMode = .normal
-                            
-                            // MARK: As Usual Set Properties What Ever Your Wish Here With Sheet Controller
-                            sheet.largestUndimmedDetentIdentifier = largestUndimmedIdentifier
-                            sheet.preferredCornerRadius = sheetCornerRadius
-                        } else {
-                            print("NO CONTROLLER FOUND")
                         }
                     }
             }
